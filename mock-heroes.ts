8fe67455307bf2aa9ffd620885831c565c3a77d6
alpsid <47097095+alpsid@users.noreply.github.com>
2020-03-12 10:34:18 +0530

    Add files via upload

---
+++ mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Avengers Endgame' },
+  { id: 12, name: 'Once upon a time in Hollywood' },
+  { id: 13, name: 'John Wick' },
+  { id: 14, name: 'Frozen 2' },
+  { id: 15, name: 'Jumanji' },
+  { id: 16, name: 'IT chapter two' },
+  { id: 17, name: 'Ford v Ferrari' },
+  { id: 18, name: 'Docter Sleep' },
+  
+];
